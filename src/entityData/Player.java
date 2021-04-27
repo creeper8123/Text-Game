c5b067d7d49bb7d3ed7a74314bbff793bfdc712d
creeper8123 <anthgb@cloud.com>
2021-04-27 11:46:45 -0500

    Moving away from static

--- src/entityData/Player.java
+++ src/entityData/Player.java
@@ -3,24 +3,170 @@ package entityData;
 
 
 import mainExecution.mainClass;
+import worldData.mapData;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Player {
-    public static int health = 100;
+    public int health;
+    public Player(){
+        this.health = 100;
+    }
 
-    public static int[] location = {0, 15, 15}; //spawn is {0, 15, 15}. Format is {Floor, x, y}
-    public static int world = 0;
 
-    public static void movePlayer() throws IOException {
-        String[] canMoveTo = moveEntity.getViableDirections(Player.location, mainClass.world).toArray(new String[0]);
+    public int[] location = {0, 15, 15}; //spawn is {0, 15, 15}. Format is {Floor, x, y}
+    public int world = 0;
+
+    public static void movePlayer(Player player) throws IOException {
+        String[] canMoveTo = moveEntity.getViableDirections(player.location, mainClass.world).toArray(new String[0]);
         int moveTo = engineInteractions.processList.chooseFromList(canMoveTo);
         switch (canMoveTo[moveTo]) {
-            case "North" -> Player.location[1] -= 1;
-            case "East" -> Player.location[2] += 1;
-            case "West" -> Player.location[2] -= 1;
-            case "South" -> Player.location[1] += 1;
+            case "North" -> player.location[1] -= 1;
+            case "East" -> player.location[2] += 1;
+            case "West" -> player.location[2] -= 1;
+            case "South" -> player.location[1] += 1;
         }
+    }
+
+    private static final String[] moveDirections = {"North", "East", "West", "South"};
+
+    public static ArrayList<String> getViableDirections(int[] entityLocation, mapData world){
+        ArrayList<String> outputData = new ArrayList<String>();
+        //Overworld
+        if(Player.world == 0){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.Overworld[entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.Overworld.length-1){ //Is east clear to walk?
+                if(world.Overworld[entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.Overworld[entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.Overworld[Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.Overworld[entityLocation[1] + 1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+            //Ice Dungeon
+        }else if(Player.world == 1){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.dungeonIce[entityLocation[0]][entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.dungeonIce[entityLocation[0]].length-1){ //Is east clear to walk?
+                if(world.dungeonIce[entityLocation[0]][entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.dungeonIce[entityLocation[0]][entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.dungeonIce[entityLocation[0]][Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.dungeonIce[entityLocation[0]][entityLocation[1]+1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+            //Fire Dungeon
+        }else if(Player.world == 2){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.dungeonFire[entityLocation[0]][entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.dungeonFire[entityLocation[0]].length-1){ //Is east clear to walk?
+                if(world.dungeonFire[entityLocation[0]][entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.dungeonFire[entityLocation[0]][entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.dungeonFire[entityLocation[0]][Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.dungeonFire[entityLocation[0]][entityLocation[1]+1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+            //Ocean Dungeon
+        }else if(Player.world == 3){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.dungeonOcean[entityLocation[0]][entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.dungeonOcean[entityLocation[0]].length-1){ //Is east clear to walk?
+                if(world.dungeonOcean[entityLocation[0]][entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.dungeonOcean[entityLocation[0]][entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.dungeonOcean[entityLocation[0]][Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.dungeonOcean[entityLocation[0]][entityLocation[1]+1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+            //Poison Dungeon
+        }else if(Player.world == 4){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.dungeonPoison[entityLocation[0]][entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.dungeonPoison[entityLocation[0]].length-1){ //Is east clear to walk?
+                if(world.dungeonPoison[entityLocation[0]][entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.dungeonPoison[entityLocation[0]][entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.dungeonPoison[entityLocation[0]][Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.dungeonPoison[entityLocation[0]][entityLocation[1]+1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+            //Final Dungeon
+        }else if(Player.world == 5){
+            if(Player.location[1] > 0){ //Is North clear to walk?
+                if(world.dungeonFinal[entityLocation[0]][entityLocation[1]-1][entityLocation[2]].isWalkable){
+                    outputData.add(moveEntity.moveDirections[0]);
+                }
+            }
+            if(Player.location[2] < world.dungeonFinal[entityLocation[0]].length-1){ //Is east clear to walk?
+                if(world.dungeonFinal[entityLocation[0]][entityLocation[1]][entityLocation[2]+1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[1]);
+                }
+            }
+            if(Player.location[2] > 0){ //Is west clear to walk?
+                if(world.dungeonFinal[entityLocation[0]][entityLocation[1]][entityLocation[2]-1].isWalkable){
+                    outputData.add(moveEntity.moveDirections[2]);
+                }
+            }
+            if (Player.location[1] < world.dungeonFinal[entityLocation[0]][Player.location[0]].length - 1) { //Is south clear to walk?
+                if (world.dungeonFinal[entityLocation[0]][entityLocation[1]+1][entityLocation[2]].isWalkable) {
+                    outputData.add(moveEntity.moveDirections[3]);
+                }
+            }
+        }
+        return outputData;
     }
 
 
